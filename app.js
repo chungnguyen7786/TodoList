7e48c964ce8804aa908cb0b686367a1e502172ba
Chung Nguyen <themask7786@gmail.com>
2021-12-30 17:56:56 +0700

    add item feature, mark completed, styling

--- app.js
+++ app.js
@@ -3,39 +3,28 @@ const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", `${__dirname}/views`);
 
+var taskList = ["Buy food", "Cook food", "Eat food"];
+
 app.get("/", (req, res) => {
   const today = new Date();
-  const currentDay = today.getDay();
-  var day;
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log(`Error: current day is equal to ${currentDay}`);
-  }
-  res.render("./list", { day });
+  const option = {
+    weekday: "long",
+    day: "numeric",
+    month: "short",
+    // year: "numeric",
+  };
+  const day = today.toLocaleString("en-Us", option);
+  res.render("./list", { day, taskList });
+});
+
+app.post("/", (req, res) => {
+  const newTask = req.body.task;
+  taskList.push(newTask);
+  res.redirect("/");
 });
 
 const port = 3000;
