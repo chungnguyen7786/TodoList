1b134528a2fbacc9f9abfa9e51ec74281136160c
Chung Nguyen <themask7786@gmail.com>
2022-01-10 00:07:05 +0700

    connect app to mongoDB, can create custom list, add/delete/checked task

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const date = require("./date");
+const _ = require("lodash");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -7,34 +8,150 @@ app.use(express.json());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", `${__dirname}/views`);
+app.locals._ = _;
 
-const taskList = ["Go shopping", "Send contract"];
-const workList = ["Send contract"];
-let path = "";
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/todosDB")
+  .then(() => console.log("MongoDB connected!"))
+  .catch((error) => console.error(error));
 
-app.get("/", (req, res) => {
-  const day = date.getDate();
-  // const day = date.getDay();
-  path = "/";
-  res.render("./list", { heading: day, list: taskList, path });
+const todoSchema = {
+  task: {
+    type: String,
+    required: true,
+  },
+  list: String, //general or work, home,...
+  checked: String, //on or ''
+};
+
+const listSchema = {
+  name: {
+    type: String,
+    required: true,
+  },
+};
+
+const Todo = mongoose.model("Todo", todoSchema);
+const List = mongoose.model("List", listSchema);
+
+app.get("/", async (req, res) => {
+  try {
+    const day = date.getDate();
+    const todoList = await Todo.find();
+    const taskList = await List.find();
+    res.render("./list", { heading: day, path: "/", todoList, taskList });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-app.post("/", (req, res) => {
+app.get("/category/:list", async (req, res) => {
+  try {
+    const listHeading = _.capitalize(_.lowerCase(req.params.list));
+    const listUrl = _.kebabCase(_.lowerCase(req.params.list));
+    const foundList = await List.findOne({ name: listHeading });
+    if (foundList) {
+      const todoList = await Todo.find({ list: listUrl });
+      const taskList = await List.find();
+      res.render("./list", {
+        heading: `${listHeading} List`,
+        path: listUrl,
+        todoList,
+        taskList,
+      });
+    } else {
+      res.send("Task list not found!");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.post("/", async (req, res) => {
   const newTask = req.body.task;
-  taskList.push(newTask);
-  res.redirect("/");
+  const path = req.body.path;
+  try {
+    if (path === "/") {
+      const newTodo = new Todo({
+        task: newTask,
+        list: "general",
+        checked: "",
+      });
+      await newTodo.save();
+      console.log("Added task successfully!");
+      res.redirect("/");
+    } else {
+      const newTodo = new Todo({
+        task: newTask,
+        list: path,
+        checked: "",
+      });
+      await newTodo.save();
+      console.log("Added task successfully!");
+      res.redirect(`/category/${path}`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-app.get("/work", (req, res) => {
-  path = "/work";
-  res.render("./list", { heading: "Work List", list: workList, path });
+app.post("/list", async (req, res) => {
+  const listName = _.capitalize(_.lowerCase(req.body.list));
+  const path = req.body.path;
+  try {
+    const foundList = await List.findOne({ name: listName });
+    if (!foundList) {
+      const newList = new List({ name: listName });
+      await newList.save();
+      console.log("List was created successfully!");
+      if (path === "/") {
+        res.redirect("/");
+      } else {
+        res.redirect(`/category/${path}`);
+      }
+    } else {
+      res.send("This list existed!");
+    }
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-app.post("/work", (req, res) => {
-  const newTask = req.body.task;
-  taskList.push(newTask);
-  workList.push(newTask);
-  res.redirect("/work");
+app.post("/delete", async (req, res) => {
+  const id = req.body.id;
+  const path = req.body.path;
+  try {
+    await Todo.findByIdAndRemove(id);
+    if (path === "/") {
+      res.redirect("/");
+    } else {
+      res.redirect(`/category/${path}`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.post("/check", async (req, res) => {
+  const id = req.body.id;
+  const path = req.body.path;
+  let check;
+  if (req.body.check) {
+    checked = "checked";
+  } else {
+    checked = "";
+  }
+  try {
+    await Todo.findByIdAndUpdate(id, { checked });
+    if (path === "/") {
+      res.redirect("/");
+    } else {
+      res.redirect(`/category/${path}`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 const port = 3000;
